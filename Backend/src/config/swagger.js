a47229fb8e57c3a2ec24a7acfeeb08ea97c1c996
Fouziya-1903 <118104880+Fouziya-1903@users.noreply.github.com>
2026-06-10 15:49:25 +0530

    CORS Fixing

--- Backend/src/config/swagger.js
+++ Backend/src/config/swagger.js
@@ -8,7 +8,7 @@ const options = {
       version: '1.0.0',
       description: 'REST API for managing tasks and users',
     },
-    servers: [{ url: 'http://localhost:5000' }],
+    servers: [{ url: process.env.Public_URL || `http://localhost:${process.env.PORT || 5000}`} ],
     components: {
       securitySchemes: {
         bearerAuth: {
